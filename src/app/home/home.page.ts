0739ec64333c5b8c56582899af81b438be510f69
aurelcamp <aureliencampistron@yahoo.fr>
2021-01-21 10:50:56 +0100

    'u'

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,36 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(
+    public alertController: AlertController,
+    private router: Router
+  ) {}
+
+  async presentAlertConfirm() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Confirmation !',
+      message: 'Etes vous sûr de vouloir changer de page ?',
+      buttons: [
+        {
+          text: 'Annuler',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            // console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Ok',
+          handler: () => {
+            // console.log('Confirm Okay');
+            this.router.navigate(['/tabs']);
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+
+  }
 
 }
