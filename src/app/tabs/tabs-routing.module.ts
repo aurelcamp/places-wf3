7c5565c8bc2cda56bd0519b51918b751c80be0c8
aurelcamp <aureliencampistron@yahoo.fr>
2021-01-21 11:34:20 +0100

    'u'

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -12,7 +12,7 @@ const routes: Routes = [
       {
         path: 'places',
         loadChildren: () => import('./places/places.module').then( m => m.PlacesPageModule)
-      }
+      },
     ]
   },
 ];
