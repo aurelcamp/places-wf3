eb32f51232ca6910d048615b0512f0e201b193ee
aurelcamp <aureliencampistron@yahoo.fr>
2021-01-22 16:59:36 +0100

    'u'

--- src/app/tabs/map/map.page.ts
+++ src/app/tabs/map/map.page.ts
@@ -18,8 +18,8 @@ export class MapPage implements OnInit {
       layers: [
         tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
       ],
-      zoom: 5,
-      center: latLng(46.879966, -121.726909)
+      zoom: 10,
+      center: latLng(48.3, 4.07)
     };
 
     setTimeout(() => this.showMap = true);
