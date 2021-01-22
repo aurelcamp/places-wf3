5afdd50bccdb43267b8f8724c8961feaa5cee179
aurelcamp <aureliencampistron@yahoo.fr>
2021-01-22 16:53:26 +0100

    'u'

--- src/app/tabs/map/map.page.ts
+++ src/app/tabs/map/map.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { latLng, tileLayer } from 'leaflet';
 
 @Component({
   selector: 'app-map',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapPage implements OnInit {
 
+  showMap = false;
+  options: any;
+
   constructor() { }
 
   ngOnInit() {
+    this.options = {
+      layers: [
+        tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
+      ],
+      zoom: 5,
+      center: latLng(46.879966, -121.726909)
+    };
+
+    setTimeout(() => this.showMap = true);
   }
 
 }
