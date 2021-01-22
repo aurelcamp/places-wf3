710425703e66d705ab815f09d4bdaad4d4c417fb
aurelcamp <aureliencampistron@yahoo.fr>
2021-01-22 16:40:16 +0100

    'u'

--- src/app/tabs/map/map.module.ts
+++ src/app/tabs/map/map.module.ts
@@ -8,12 +8,15 @@ import { MapPageRoutingModule } from './map-routing.module';
 
 import { MapPage } from './map.page';
 
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    MapPageRoutingModule
+    MapPageRoutingModule,
+    LeafletModule
   ],
   declarations: [MapPage]
 })
